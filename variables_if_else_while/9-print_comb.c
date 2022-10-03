56b19ba8451bf6d9aa006c524d0a1a1ac9b8afe8
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-03 21:00:34 +0200

    print 0__9 possible comnination

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print 0-9 sparare with ',' and space use putchar
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
