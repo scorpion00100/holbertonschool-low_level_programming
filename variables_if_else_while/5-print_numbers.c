4aadd08741b220be998038bd8b771f7fd0014598
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-03 15:11:30 +0200

    print number 0__10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print number 0__10
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		printf(n);
+		n++;
+	}
+	printf('\n');
+
+	return (0);
+}
