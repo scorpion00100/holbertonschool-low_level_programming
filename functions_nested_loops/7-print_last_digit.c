d3e7f998b457445fa7a79a9c08ae6a527bc37efc
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-04 16:12:13 +0200

    bauer

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,13 @@
  * Return: last digit of number
  */
 
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	if (n < 0)
+	{
 		n *= -1;
 
-	_putchar('0' + (n % 10));
-
+       	_putchar('0' + (n % 10));
+	}
 	return (n % 10);
 }
