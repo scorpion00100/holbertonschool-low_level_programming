79c869e81fb328578a578744a0e5001b3a2570fe
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-04 14:21:22 +0200

    print _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,9 +7,15 @@
 
 int main(void)
 {
-	char n = "_putchar";
-
-	_putchar("%c", n);
+	_putchar('_');
+	_puthcar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
