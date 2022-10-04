66d6308f22a5f1a93be8dff46f3d484b81a479fe
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-04 17:09:32 +0200

    last num

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,15 @@
 /**
  * print_last_digit - print last digit of integer
  * @n: integer argument
- * Return: last digit of number
+ * Return: last of number
  */
 
 int print_last_digit(int n)
 {
-	if (n < 0)
+	while (n < 0)
 	{
+		_putchar('0' + (n % 10));
 		n *= -1;
-
-       	_putchar('0' + (n % 10));
 	}
 	return (n % 10);
 }
