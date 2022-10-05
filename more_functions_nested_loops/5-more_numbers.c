e168e25997e76c88afe86e56c9b71d0cf7e374ea
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-06 00:43:05 +0200

    print ten times num 0__14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the num from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int n, c;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (n = 0; n <= 14 n++)
+		{
+			if (n / 10 > 0)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
