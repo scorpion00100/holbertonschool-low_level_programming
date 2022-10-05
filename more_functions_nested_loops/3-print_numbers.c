427c74a044cdba3f7acbf88608527b3703a2bbdf
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-06 00:12:51 +0200

    print num 0__9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print num from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\');
+}
