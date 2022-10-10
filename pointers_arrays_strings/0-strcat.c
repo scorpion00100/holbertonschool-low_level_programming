ecfb98fc25fa68ea5ba4ebe6bb7084e56aef82c5
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-11 00:55:48 +0200

    concatenate

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src with @dest
- * @dest: A pointer to the string to be concatenated upon
- * @src: The source string to be appended to @dest
- * Return: A pointer to the destination string @dest
+ * _strcat - concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
  */
-char *strcat(char *dest, const char *src)
+
+char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int j = 0;
