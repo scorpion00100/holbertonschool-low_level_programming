1b75a2a8c393de0c1b74e398e50a991a5f2497e3
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-13 23:28:44 +0200

    gets lenght of a prefix

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0, j;
 	int matching = 0;
 
-	while (s[i] != '\0') /*iterate through string*/
+	while (s[i] != '\0')
 	{
 
 		for (j = 0; accept[j] != '\0'; j++)
