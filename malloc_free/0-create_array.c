7ec29f2f902db3231aa1e1184aa07fdd24a6cd90
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-24 10:05:05 +0200

    create an array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 
+
 /**
  * create_array - create array of chars and initialize with a specific char
  * @size: size of array
@@ -22,7 +23,7 @@ char *create_array(unsigned int size, char c)
 	if (tab == NULL)
 		return (NULL);
 
-	for (i = 0; i < size ; i++)
+	for (i = 0; i < (int)size ; i++)
 	{
 		*(tab + i) = c;
 	}
