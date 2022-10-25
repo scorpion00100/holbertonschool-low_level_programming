6af7ac0ff552dbf8c7e111bd924dfe24ba2700c0
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-25 16:18:57 +0200

    concatenate 2 str wiht malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -34,23 +34,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+
 	if (num < 0)
 		return (NULL);
+
 	if (num >= _strlen(s2))
 		num = _strlen(s2);
 
 	len = _strlen(s1) + num + 1;
 
 	ptr = malloc(sizeof(*ptr) * len);
+
 	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
+
 	for (j = 0; j < num; j++)
 		ptr[i + j] = s2[j];
+
 	ptr[i + j] = '\0';
 
 	return (ptr);
