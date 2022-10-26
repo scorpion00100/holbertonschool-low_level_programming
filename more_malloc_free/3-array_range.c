5296f68724c0950a20da89ea22bb9ac534ec7eb7
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-26 07:39:32 +0200

    min and max

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include <stdlib>
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers.
+ * @min: first int number
+ * @max: last int number
+ *
+ * Return: ptr to new allaoc Mo
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int d;
+	int b = (max - min + 1);
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * b);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (d = 0; d < n; d++)
+		ptr[i] = min++;
+
+	return (ptr);
+}
