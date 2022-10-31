dc450501806bbbe4016523de4d8bab18089e904c
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-31 15:23:52 +0100

    initialize value and print

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialize the specific struct dog with vzlues
@@ -11,7 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-(*d).name = name;
-(*d).age = age;
-(*d).owner = owner;
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
