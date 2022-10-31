52236c3dee698c5535aea3a8d7d89204c007049b
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-31 15:28:17 +0100

    initialize value and print

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,16 +17,4 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-/**
-* struct dog_t - Def the dog structure
-* @name: Name of the dog
-* @age: The age of the dog
-* @owner: The name of the owner of the dog
-*/
-
-typedef struct dog dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 #endif
