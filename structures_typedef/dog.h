3dc2a5f00f01c95233d956b9f32cb99523ba7c0f
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-31 10:06:04 +0100

    adding struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dogs
+ * @name: first mber of stuct
+ * @age: 2nd mber of sturct
+ * @owner: 3ird mber of sturct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
