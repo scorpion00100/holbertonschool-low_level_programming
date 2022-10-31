32af0c48c72251e581b0f8d8820004aa55c137d7
scorpion00100 <danbetheliryivuze@gmail.com>
2022-10-31 15:12:50 +0100

    initialize value

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize the specific struct dog with vzlues
+ * @d: the dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
