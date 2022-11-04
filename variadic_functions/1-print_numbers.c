c60d12d30fd352cc2d22f3fd637a6c95208151f3
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-04 10:39:55 +0100

    new prototypes and print list number

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print  numb
+ * @separator: delimit
+ * @n: amount arguments in list
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list db;
+	unsigned int d;
+
+	if (n > 0)
+	{
+		va_start(db, n);
+		for (d = 1; d <= n; d++)
+		{
+			printf("%d", va_arg(db, int));
+
+			if (d != n && separator != NULL)
+			{
+				printf("%s", separator);
+			}
+		}
+		va_end(db);
+	}
+	printf("\n");
+}
