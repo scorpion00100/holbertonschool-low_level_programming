aecfb79df13423306824905edb18e4ff0bfa8328
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-15 11:56:17 +0100

    add element to the back

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a new node at the end of this list
+ * @head: pointer to head in main function
+ * @str: string to be duplicated
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+	list_t *current = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = new;
+
+	return (new);
+}
