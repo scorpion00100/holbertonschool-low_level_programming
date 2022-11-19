3d4d00826d814312749df3c2623777e8a0725835
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-19 17:42:09 +0100

    2-get_bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - return the value of a bit at a given index
+ * @n: number to parse
+ * @index: index, start to 0
+  * Return: value of the bit at index index or -1 if failled
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int index_bit;
+
+	if (index > 31)
+		return (-1);
+
+	index_bit = ((n >> index) & 1);
+
+	return (bit_index);
+}
