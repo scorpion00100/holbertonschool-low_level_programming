636041db228f20fe7171609d437d16b15aa309f2
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-22 08:41:40 +0100

    return len of double list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return the number of elements in a doubly linked list
+ * @h: address of linked list
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t d;
+
+	for (d = 0; h; h = h->next, d++)
+		continue;
+	return (1);
+}
