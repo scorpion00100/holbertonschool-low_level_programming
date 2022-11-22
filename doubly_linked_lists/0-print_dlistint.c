c63676c65747e0bbeeaa0c6caca16570c613b7cf
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-22 08:48:21 +0100

    print all elements of lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t d;
 
-	for (d = 0; h; h = h->next, d++)
-		printf("%d", h->n);
+	for (d = 0; h!= NULL; h = h->next, d++)
+		printf("%d\n", h->n);
 	return (d);
 }
