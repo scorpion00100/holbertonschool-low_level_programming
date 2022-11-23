e0724f51ab4ff6e6cc2d3560f79be2587fa73d5f
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-23 10:03:09 +0100

    add new_element at begin

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (head == NULL)
 		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -20,10 +21,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head != NULL)
 	{
 		while ((*head)->prev != NULL)
-		{
 			*head = (*head)->prev;
-			(*head)->prev = new;
-		}
+		(*head)->prev = new;
 	}
 	new->n = n;
 	new->prev = NULL;
