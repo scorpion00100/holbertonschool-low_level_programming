56660495828a71b961b2feb8a70fd734a0981e36
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-24 00:42:28 +0100

    return sum all data (n) of linked list

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * sum_dlistint - Sum of all the data (n) of a dlistint_t linked list
  *
- * @head: Head of the list
+ * @head: head of the list
  *
  * Return: Sum of all the nodes value
  **/
