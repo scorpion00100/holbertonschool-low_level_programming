218c635ebf3302d5ed49315fe7e4e036026ffe62
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-26 21:18:35 +0100

    add element into the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(ht->array[d]->key, key) == 0)
 		{
-			free(ht->array[i]->value);
+			free(ht->array[d]->value);
 			ht->array[d]->value = strdup(value);
 			return (1);
 		}
