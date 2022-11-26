4d08b709ec57eaaaf620f47b9cf7fa2191c51879
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-26 21:38:43 +0100

    delete hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Delete a hash table
+ * @ht: Hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+
+	for (d = 0; d < ht->size; d++)
+	{
+		if (ht->array[d] != NULL)
+		{
+			node = ht->array[d];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
