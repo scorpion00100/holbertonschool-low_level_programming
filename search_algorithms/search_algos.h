bd80fbfeb2270e520ab5bec838322f3a220f7ca6
scorpion00100 <danbetheliryivuze@gmail.com>
2023-01-04 13:43:02 +0100

    prototypes_file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCHING_ALGORITHMS_H
+#define SEARCHING_ALGORITHMS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
